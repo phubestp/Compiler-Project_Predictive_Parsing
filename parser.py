e4dad6f2ba4880ec879aefe27c3ddfd176b17c93
phubestp <phubest.p@ku.th>
2024-02-23 04:15:57 +0700

    comment and remove redundant code

--- parser.py
+++ parser.py
@@ -1,3 +1,5 @@
+# parsing table 
+# { 'non-terminal': {'terminal' : production... } ,... }
 parsing_table = {
     'S': {
         '(': 'S->(S)S',
@@ -8,46 +10,58 @@ parsing_table = {
 
 input_string = input("Input string: ")
 input_string = input_string + '$'
-input_string = [*input_string]
+input_string = [*input_string] # convert string to list
 
-parsing_stack = '$' + list(parsing_table.keys())[0]
-parsing_stack = [*parsing_stack]
+parsing_stack = '$' + list(parsing_table.keys())[0] # put initial grammar
+parsing_stack = [*parsing_stack] # convert string to list
 
 top = len(parsing_stack) - 1
+# following algorithm, x is top of parsing_stack and a is the first input symbol
 x = parsing_stack[top]
 a = input_string[0]
 
+# header
 print('Parsing Stack\tInput\tActions')
 
 while x != '$':
-    action = ""
+
+    # row formatting
     d = ''.join(parsing_stack) + "\t\t" + ''.join(input_string) + "\t"
+
+    # if top of parsing_stack and input is match, pop both from stacks
     if x == a:
-        action = "match"
         print(d + "match")
         parsing_stack.pop(top)
         input_string.pop(0)
+
+    # if x is terminal, it's error
     elif x not in list(parsing_table.keys()):
         print("--Error--")
         break
+
+    # if it have production M[x,a], put reverse production to the parsing stack
     elif parsing_table[x][a] is not None:
-        action = parsing_table[x][a]
         print(d + parsing_table[x][a])
-        p = parsing_table[x][a].split('->')[1]
-        p = [*p]
+        p = parsing_table[x][a].split('->')[1] # split -> from production
+        p = [*p] # convert string to list
         p.reverse()
         parsing_stack.pop(top)
-        parsing_stack += p
-        action = parsing_table[x][a]
+        parsing_stack += p # put reverse produciton
         if 'ε' in parsing_stack:
             parsing_stack.remove('ε')
+
+    # if it not have production M[x,a], it's error too
     else:
         print("--Error--")
         break
 
+    # update top, x and a
     top = len(parsing_stack) - 1
     x = parsing_stack[top]
     a = input_string[0]
 
-if x == '$' and a == '$': print(''.join(parsing_stack) + "\t\t" + ''.join(input_string) + "\t" + "accept")
-else: print(''.join(parsing_stack) + "\t\t" + ''.join(input_string) + "\t" + "reject")
+# if x and a is $ => accept 
+if x == '$' and a == '$': 
+    print(''.join(parsing_stack) + "\t\t" + ''.join(input_string) + "\t" + "accept")
+else: 
+    print(''.join(parsing_stack) + "\t\t" + ''.join(input_string) + "\t" + "reject")
